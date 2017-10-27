2669dd6bdad689053fc3d0ccf4d5c0a4dd56c841
cokepluscarbon <93948886@qq.com>
2017-10-28 03:49:13 +0800

    install 脚本

--- tools/install.sh
+++ tools/install.sh
@@ -4,5 +4,7 @@ chmod +x shadowsocks.sh
 
 wget --no-check-certificate -O /etc/shadowsocks.json https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/shadowsocks.json
 
-echo "/etc/init.d/shadowsocks start --manager-address 127.0.0.1:1234 -c /etc/shadowsocks.json" > sss_run.sh
+echo "/etc/init.d/shadowsocks start --manager-address 127.0.0.1:1234 -c /etc/shadowsocks.json 2>&1> /dev/null" > sss_run.sh
 chmod +x sss_run.sh
+
+echo "nohup java -jar osecret-kikit.jar --server.host=97.64.83.111 --spring.kafka.consumer.group-id=97.64.83.111 &" > kikit_run.sh
