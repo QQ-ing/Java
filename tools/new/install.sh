f88d7ec37a435455630dc508a3d6edac05227436
cokepluscarbon <93948886@qq.com>
2017-10-30 01:52:57 +0800

    update

--- tools/new/install.sh
+++ tools/new/install.sh
@@ -18,7 +18,7 @@ echo 'write to /etc/rc.d/rc.local'
 echo 'ssserver --manager-address $(hostname -I | cut -d" " -f 1):1234 -c /etc/shadowsocks.json -d start' >> /etc/rc.d/rc.local
 
 echo 'download kikit.jar'
-echo --no-check-certificate -O /etc/kikit.jar https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/new/osecret-kikit.jar
+echo --no-check-certificate -O /etc/kikit.jar https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/osecret-kikit.jar
 
 echo 'create kikit_run.sh'
 echo 'java -jar osecret-kikit.jar --server.host=$(hostname -I | cut -d" " -f 1) --spring.kafka.consumer.group-id=$(hostname -I | cut -d" " -f 1) 2>&1> /dev/null' > kikit_run.sh
