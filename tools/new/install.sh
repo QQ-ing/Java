9154ffa4ba22a098731d835497ab753f1a6fb707
cokepluscarbon <93948886@qq.com>
2017-10-30 01:40:09 +0800

    优化 ss脚本

--- tools/new/install.sh
+++ tools/new/install.sh
@@ -7,7 +7,24 @@ echo 'start download shadowsocks.json'
 wget --no-check-certificate -O /etc/shadowsocks.json https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/new/shadowsocks.json
 echo 'finish download shadowsocks.json'
 
-echo 'start download ssserver.sh'
-wget --no-check-certificate -O /etc/shadowsocks.json https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/new/ssserver.sh
-chmod +x ssserver.sh
-echo 'finish download ssserver.sh'
+echo 'create sss_run.sh'
+echo 'ssserver --manager-address $(hostname -I | cut -d" " -f 1):1234 -c /etc/shadowsocks.json -d start' > sss_run.sh
+chmod +x sss_run.sh
+
+echo 'write to /etc/rc.d/rc.local'
+echo 'ssserver --manager-address $(hostname -I | cut -d" " -f 1):1234 -c /etc/shadowsocks.json -d start' >> /etc/rc.d/rc.local
+
+echo 'download kikit.jar'
+echo --no-check-certificate -O /etc/kikit.jar https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/new/osecret-kikit.jar
+
+echo 'create kikit_run.sh'
+echo 'java -jar osecret-kikit.jar --server.host=$(hostname -I | cut -d" " -f 1) --spring.kafka.consumer.group-id=$(hostname -I | cut -d" " -f 1) 2>&1> /dev/null' > kikit_run.sh
+chmod +x kikit_run.sh
+
+echo 'write to /etc/rc.d/rc.local'
+echo 'java -jar osecret-kikit.jar --server.host=$(hostname -I | cut -d" " -f 1) --spring.kafka.consumer.group-id=$(hostname -I | cut -d" " -f 1) 2>&1> /dev/null' >> /etc/rc.d/rc.local
+
+echo 'start sserver'
+sh sss_run.sh
+echo 'start kikit'
+sh kikit_run.sh
--- tools/new/ssserver.sh
+++
@@ -1,5 +0,0 @@
-$(hostname -I | cut -d" " -f 1) 
-
-echo 'start ssserver'
-ssserver --manager-address $(hostname -I | cut -d" " -f 1) :1234 -c /etc/shadowsocks.json -d start
-echo 'ssserver ok'
