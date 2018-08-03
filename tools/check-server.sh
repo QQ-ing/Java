702c6476d870ecefdc502b03c2362ec1e58b7949
cokepluscarbon <93948886@qq.com>
2018-08-03 18:15:38 +0800

    Update check-server.sh

--- tools/check-server.sh
+++ tools/check-server.sh
@@ -3,3 +3,11 @@ then
     echo "check sss was stoped, restart sss at `date`" >>  /var/log/checksss.log
     ssserver --manager-address=127.0.0.1:1234 -c /etc/shadowsocks.json -d restart
 fi
+
+COUNT=$(ps ax | grep kikit.jar | grep java | wc -l)
+if [ $COUNT -eq 0 ]; then
+    echo 'start kikit'
+    sh /opt/kikit/start.sh
+else
+    echo 'kikit is running'
+fi
