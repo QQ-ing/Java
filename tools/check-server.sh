23a12c39470f41786ae28909ca51d084b4ad8fe6
cokepluscarbon <93948886@qq.com>
2019-08-15 03:02:11 +0800

    Update check-server.sh

--- tools/check-server.sh
+++ tools/check-server.sh
@@ -1,7 +1,7 @@
 count=$(ps -ef | grep manager-address |grep -v "grep" |wc -l)
 if [ $count -eq 0 ];then
     echo "check sss was stoped, restart sss at `date`" >>  /var/log/checksss.log
-    ssserver --manager-address=127.0.0.1:1234 -c /etc/shadowsocks.json -d restart
+    sudo ssserver --manager-address=127.0.0.1:1234 -c /etc/shadowsocks.json -d restart
 else
     echo "ssserver -s running"
 fi
@@ -9,7 +9,7 @@ fi
 COUNT=$(ps ax | grep kikit.jar | grep java | wc -l)
 if [ $COUNT -eq 0 ];then
     echo "start kikit"
-    sh /opt/kikit/start.sh
+    sudo sh /opt/kikit/start.sh
 else
     echo "kikit is running"
 fi
@@ -17,7 +17,7 @@ fi
 SIZE=$(du /opt/kikit/logs/ | cut -f 1)
 if [ $SIZE -gt 2097152 ];then
    echo "delete logs in /opt/kikit/logs/2*"
-   rm -rf /opt/kikit/logs/2*
+   sudo rm -rf /opt/kikit/logs/2*
 else
     echo "not need to delete logs"
 fi
