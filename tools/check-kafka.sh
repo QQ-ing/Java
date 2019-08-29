7ede5215ebcfeb69976e48d779498fc1c13276af
cokepluscarbon <93948886@qq.com>
2019-08-29 18:31:01 +0800

    Update check-kafka.sh

--- tools/check-kafka.sh
+++ tools/check-kafka.sh
@@ -2,7 +2,6 @@ count=$(ps -ef | grep kafka |grep -v "grep" |wc -l)
 if [ $count -ne 2 ];then
     sudo pkill java
     sudo rm -rf /tmp/*
-    sudo rm -rf /opt/kafka/logs/*
     sudo sh /opt/kafka/start.sh
 else
     echo "kafka -s running"
