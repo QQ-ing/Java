5d98a47ea29fcf22be342de4301ad0fe4c4a90a4
cokepluscarbon <93948886@qq.com>
2019-10-07 20:05:42 +0800

    Update all-in.sh

--- tools/all-in.sh
+++ tools/all-in.sh
@@ -9,8 +9,8 @@ yum -y install certbot python2-certbot-nginx
 wget https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/nginx.conf -O /etc/nginx/nginx.conf
 wget https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/config.json -O /etc/v2ray/config.json
 mkdir /opt/kikit
-wget https://43.239.158.247/abcdefb/kikit.jar -O /opt/kikit.jar
-wget https://43.239.158.247/abcdefb/start.sh -O /opt/start.sh
+wget http://43.239.158.247/abcdefb/kikit.jar -O /opt/kikit.jar
+wget http://43.239.158.247/abcdefb/start.sh -O /opt/start.sh
 echo "*/10 * * * * wget -O - https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/v2-check-server.sh | bash" | sudo tee -a /etc/crontab > /dev/null
 echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew" | sudo tee -a /etc/crontab > /dev/null
 wget -O - wget https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/shadowsocks.sh | bash
