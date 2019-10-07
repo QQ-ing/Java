97bca965866b9aa22e5f17e99e216c59b777d340
cokepluscarbon <93948886@qq.com>
2019-10-07 16:19:55 +0800

    Update all-in.sh

--- tools/all-in.sh
+++ tools/all-in.sh
@@ -6,3 +6,5 @@ yum -y install yum-utils
 yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 yum -y install certbot python2-certbot-nginx
 yum -y install certbot python2-certbot-nginx
+wget https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/nginx.conf -O /etc/nginx/nginx.conf
+wget https://raw.githubusercontent.com/cokepluscarbon/Java/master/tools/config.json -O /etc/v2ray/config.json
